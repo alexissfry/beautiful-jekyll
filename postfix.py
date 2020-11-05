3920f5f2d9248e0629ccc2b201ea43bc9b26639f
alexissfry <afry477@gmail.com>
2020-11-05 15:52:25 -0500

    Added question mark

---
+++ postfix.py
@@ -0,0 +1,13 @@
+import csv 
+
+with open('calculate_me.csv', 'r') as f: 
+    data = csv.reader(f)
+    for row in data:
+
+def 
+
+# if s == "A" or s == "B" or s == "C":
+# if s in ["A","B","C","D", "E"]:
+# int(word)
+# str(avg)
+#"{}", format(avg)
