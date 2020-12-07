5cebd3e6b1d653c4a768e27dd2ebd37fb95b9065
alexissfry <afry477@gmail.com>
2020-12-06 23:22:00 -0500

    additions

--- _posts/2020-12-06-hospital-lab.md
+++ _posts/2020-12-06-hospital-lab.md
@@ -2,11 +2,27 @@
 #Frontmatter
 layout: post
 title: Hospital Lab
-description: AI in Healthcare
 ---
 
 # My Process
-## How
+### Main Steps I Took
+1. Query the server to generte a CSV with raw data.
+2. Clean and standarize the data so I can find what I'm looking for: which county in NY state has the most hospital beds per person. This meant that I wanted to convert XHAB to 1HAB for each county, and then I want to find the max value in the beds column of the cleaned CSV. 
+3. I then did an analysis of the cleaned CSV as described above to find which county in NY state has the most hospital beds per person.  
 
-## Logistics 
-In terms of who I worked with, I worked through code and errors in a breakout room with Alex, David, Lucca, and Ethan on Thursday. I asked Harry about standardizing the data in the csv file and checked my final answer for the county with the most hospital beds with him as well. I answered a few of Aidan's questions about creating the csv file.
+### The How & Setbacks
+I knew that I had to access the API using similar techniques as the weather API classwork, so I approached this lab with the same techniques. There were two differences: 1) I had to get the "id"th row from the API that was accomplished with my getRow function and 2) I tested the data retreval with the status code 200, which indicates the request has succeeded otherwise it throws an error. Figuring out how to get the "id"th row rather than just inputting a known datapoint like in the weather API took me a minute to figure out, but I eventually got it during class. 
+
+Now since I could access the data, I wanted to put it into a CSV. However, I realized that the data was plain text and not json like the weather API shown down below. So, I had to create a function I called rewrite to convert the plain text into a list, so then I could start cleaning and analyzing the raw data.
+
+#### Plain Text 
+![](plaintext.png)
+
+#### JSON
+![](json.png)
+
+### Results 
+I ended up finding that New York county has the most hospital beds per person. Not surprising :(
+
+### Logistics 
+In terms of who I worked with, I worked through code and errors in a breakout room with Alex, David, Lucca, and Ethan on Thursday. I asked Harry questions and checked my final answer for the county with the most hospital beds with him as well. I answered a few of Aidan's questions about creating the csv file.
--- hospital_lab.py
+++ hospital_lab.py
@@ -7,7 +7,7 @@ key = "ArtOfDataKEY123"
 
 
 #pull a given row number from the API
-def getData(id):
+def getRow(id):
     payload = {
         'key': key,
         'id': str(id)
@@ -23,12 +23,20 @@ def getData(id):
 def rewrite(string):
     line = []
     index = 0
-    for i in range(0,len(string)):
+    for i in range(0, len(string)):
         if string[i:i+1] == ",":
             line.append(string[index:i])
             index = i+1
     line.append(string[index:])
     return line
+
+#getting the data from the API and writing it into a csv I called hospital_raw.csv
+with open('hospital_raw.csv', 'w', newline = '') as f:
+    writing = csv.writer(f)
+    for i in range(0,135):
+        string = getRow(i)
+        row = rewrite(string)
+        writing.writerow(row)
 
 #standardizing measurements by changing measure to 1HAB and adjusting beds accordingly
 def standardize(list):
@@ -36,34 +44,24 @@ def standardize(list):
     list[5] = float(list[5])/measure
     list[4] = "1HAB"
     return list
-
-#getting the data from the API and writing it into a csv I called hospital_raw.csv
-with open('hospital_raw.csv', 'w', newline = '') as f:
-    writing = csv.writer(f)
-    for i in range(0,135):
-        string = getData(i)
-        row = rewrite(string)
-        writing.writerow(row)
         
-#get all the standardized data and write into hospital_lab.csv       
+#geting all the standardized data and writing it into hospital_lab.csv which will be ready for analysis     
 with open('hospital_lab.csv', 'w', newline = '') as f:
     writing = csv.writer(f)
-    writing.writerow(rewrite(getData(0)))
+    writing.writerow(rewrite(getRow(0)))
     for i in range(1,135):
-        string = getData(i)
+        string = getRow(i)
         row = rewrite(string)
         standardized_row = standardize(row)
         writing.writerow(standardized_row)
 
-#open hospital_lab.csv
+#open hospital_lab.csv, organizing data by county (no matter the bed type), creating a dictionary for each country
 counties = {}
 with open("hospital_lab.csv", "r") as f:
     data = csv.reader(f)
     next(data)
     for row in data:
-        #organize data by county, regardless of bed type
         county = row[2]
-        #create dictionary for new county
         if county not in counties:
             counties[county] = []
         counties[county].append(row[5])
@@ -75,15 +73,15 @@ def summation(list):
         a = a + float(i)
     return a
 
-#create a list with the sum of beds/person for each county
+#creating a list with the sum of beds per person for each county
 beds = []
 for i in counties:
     a = summation(counties[i])
     counties[i].append(a)
-    bed_sum.append(a)
+    beds.append(a)
 
 
-#find max and print result
+#finding the number max out of beds and print the result 
 maximum = max(beds)
 county = ""
 for i in counties:
